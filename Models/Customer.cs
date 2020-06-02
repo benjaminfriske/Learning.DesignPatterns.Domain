6a9fcdc53e748d8b81129ddc00bf09ab6848e494
Benjamin Friske <benjaminfriske@gmail.com>
2020-06-02 13:34:11 -0500

    Added Customer, Order, User class.

---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learning.DesignPatterns.Domain.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learning.DesignPatterns.Domain.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
